18c2385a32e4c692783d0abebeace310d8202793
archieyang <overflow.yang@gmail.com>
2014-01-13 16:52:40 +0800

    add get-card

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,19 +1,34 @@
 (ns app.core)
 
-(def card_type '("Spade" "Club" "Diamond" "Heart"))
-(def card_number 
+(def card-type '("Spade" "Club" "Diamond" "Heart"))
+(def card-val 
 		(concat  "A" (range 2 11) '("J" "Q" "K")))
-(def cards
-		(vec 
-			(for [type card_type 
-				num card_number
-				:let [res (hash-map :type type :value num)]]
-				res)))
+(def used-cards '())
+(def deck-of-cards 
+	(for [t card-type n card-val]
+		{:type t :value n}))
+
+
+(defn rand-card-int []
+	(rand-int (count deck-of-cards)))
+
+(defn get-card []
+	(nth  deck-of-cards (rand-card-int)))
+
+(defn put-card-into-list[x-list card]
+	(conj x-list card))
+
 		
 (defn main [] 
 	(do
 		(println "Hello world")
-		(println card_type)
-		(println card_number)
-		(println (count cards))
-		(println (:value (nth cards 0)))))
+		(println card-type)
+		(println card-val)
+		(println (count deck-of-cards))
+		(let [card (get-card) u-c '()]
+			(do 
+				(put-card-into-list used-cards card)
+				(println card)
+				(conj u-c 1)
+				(println u-c)
+				(println (conj used-cards card))))))
